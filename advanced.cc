0039dafa18789da2d4d524402ac7561bd19635f8
Charlie850916 <charlie1chuang@gmail.com>
2018-10-17 02:58:17 +0800

    10/17

--- advanced.cc
+++ advanced.cc
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-
 int main(int argc, char* argv[])
 {
 	ios_base::sync_with_stdio(false);
@@ -15,7 +14,7 @@ int main(int argc, char* argv[])
 	MPI_Init(&argc, &argv);
       	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-
+	
 	dataSize = leftSize = rightSize = n/size;
 	tmp = n - dataSize*size;
 	base = dataSize*rank;
@@ -45,73 +44,86 @@ int main(int argc, char* argv[])
 
 	sort(data, data+dataSize);
 
-	sorted = 0;
-	MPI_Request req1, req2;
+	MPI_Request req1, req2, req3, req4;
 
+	sorted = 0;
 	while(!sorted)
     	{
 		sorted = 1;
-		for(odd_even = 0 ; odd_even < 2 ; ++odd_even)
+		for(odd_even = 0 ; odd_even < 2 && rank<size ; ++odd_even)
 		{
-			if(rank<size)
+			if(rank%2==odd_even)
 			{
-        			if(rank%2==odd_even)
+				if(rank<size-1)
 				{
-					if(rank<size-1)
+					MPI_Isend(data+dataSize/2, dataSize/2+dataSize%2, MPI_FLOAT, rank+1, 0, MPI_COMM_WORLD, &req1);
+					MPI_Isend(data, dataSize/2, MPI_FLOAT, rank+1, 1, MPI_COMM_WORLD, &req3);
+
+					MPI_Irecv(right, rightSize/2+rightSize%2, MPI_FLOAT, rank+1, 0, MPI_COMM_WORLD, &req2);
+					MPI_Irecv(right+rightSize/2+rightSize%2, rightSize/2, MPI_FLOAT, rank+1, 1, MPI_COMM_WORLD, &req4);
+						
+					MPI_Wait(&req2, MPI_STATUS_IGNORE);
+
+					if(right[0] < data[dataSize-1])
 					{
-						MPI_Isend(data, dataSize, MPI_FLOAT, rank+1, 0, MPI_COMM_WORLD, &req1);
-						MPI_Irecv(right, rightSize, MPI_FLOAT, rank+1, MPI_ANY_TAG, MPI_COMM_WORLD, &req2);
-						MPI_Wait(&req2, MPI_STATUS_IGNORE);
-						if(right[0] < data[dataSize-1])
+						sorted = cnt = id = rid = 0;
+						while(cnt < dataSize)
 						{
-							sorted = cnt = id = rid = 0;
-							while(cnt < dataSize)
-							{
-								if(rid>=rightSize)
-									a[cnt++] = data[id++];
-								else if(id>=dataSize)
-									a[cnt++] = right[rid++];
-								else if(data[id] <= right[rid])
-									a[cnt++] = data[id++];
-								else
-									a[cnt++] = right[rid++];
-							}
-							ttmp = data;
-							MPI_Wait(&req1, MPI_STATUS_IGNORE);
-							data = a;
-							a = ttmp;
+							if(rid==rightSize/2)
+								MPI_Wait(&req4, MPI_STATUS_IGNORE);
+							if(rid>=rightSize)
+								a[cnt++] = data[id++];
+							else if(id>=dataSize)
+								a[cnt++] = right[rid++];
+							else if(data[id] < right[rid])
+								a[cnt++] = data[id++];
+							else
+								a[cnt++] = right[rid++];
 						}
-					}
-				}
-				else
-				{
-					if(rank > 0)
-					{
-						MPI_Isend(data, dataSize, MPI_FLOAT, rank-1, 0, MPI_COMM_WORLD, &req1);
-						MPI_Irecv(left, leftSize, MPI_FLOAT, rank-1, MPI_ANY_TAG, MPI_COMM_WORLD, &req2);
-						MPI_Wait(&req2, MPI_STATUS_IGNORE);
-						if(data[0] < left[leftSize-1])
-						{
-							sorted = 0, id = cnt = dataSize-1, lid = leftSize-1;
-						        while(cnt >= 0)
-							{
-								if(lid<0)
-									a[cnt--] = data[id--];
-								else if(id<0)
-									a[cnt--] = left[lid--];
-								else if(left[lid] < data[id])
-									a[cnt--] = data[id--];
-								else
-									a[cnt--] = left[lid--];
-							}
-							ttmp = data;
-							MPI_Wait(&req1, MPI_STATUS_IGNORE);
-							data = a;
-							a = ttmp;
-						}
+						ttmp = data;
+						MPI_Wait(&req1, MPI_STATUS_IGNORE);
+						MPI_Wait(&req3, MPI_STATUS_IGNORE);
+						data = a;
+						a = ttmp;
 					}
 				}
 			}
+			else
+			{
+				if(rank > 0)
+				{
+					MPI_Isend(data, dataSize/2+dataSize%2, MPI_FLOAT, rank-1, 0, MPI_COMM_WORLD, &req1);
+					MPI_Isend(data+dataSize/2+dataSize%2, dataSize/2, MPI_FLOAT, rank-1, 1, MPI_COMM_WORLD, &req3);
+
+					MPI_Irecv(left+leftSize/2, leftSize/2+leftSize%2, MPI_FLOAT, rank-1, 0, MPI_COMM_WORLD, &req2);
+					MPI_Irecv(left, leftSize/2, MPI_FLOAT, rank-1, 1, MPI_COMM_WORLD, &req4);
+
+					MPI_Wait(&req2, MPI_STATUS_IGNORE);
+
+					if(data[0] < left[leftSize-1])
+					{
+						sorted = 0, id = cnt = dataSize-1, lid = leftSize-1;
+					        while(cnt >= 0)
+						{
+							if(lid==leftSize/2)
+								MPI_Wait(&req4, MPI_STATUS_IGNORE);
+							if(lid<0)
+								a[cnt--] = data[id--];
+							else if(id<0)
+								a[cnt--] = left[lid--];
+							else if(left[lid] < data[id])
+								a[cnt--] = data[id--];
+							else
+								a[cnt--] = left[lid--];
+						}
+						ttmp = data;
+						MPI_Wait(&req1, MPI_STATUS_IGNORE);
+						MPI_Wait(&req3, MPI_STATUS_IGNORE);
+						data = a;
+						a = ttmp;
+					}
+				}
+			}	
 		}
 		send = sorted;
 		MPI_Allreduce(&send, &sorted, 1, MPI_CHAR, MPI_LAND, MPI_COMM_WORLD);
